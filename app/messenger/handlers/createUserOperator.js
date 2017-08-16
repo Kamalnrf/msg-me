3d50d0d8a2e5db9369ad460c5748f237834f22b3
Kamal Mukkamala <Kamalnrf@yahoo.com>
2017-08-17 05:19:02 +0530

    Added image sharing.

--- app/messenger/handlers/createUserOperator.js
+++ app/messenger/handlers/createUserOperator.js
@@ -43,11 +43,9 @@ const newUser = {
                                                                     {type:"web_url", url: 'http://m.me/1536772976386751', title: 'Open Bot'}
                                                                 ]
                                                             },
-                                                        ]);
-                                                    });
-
-                                                convo.say(`Thanks for creating the username. Your username is ${reqUserName}. Share it among your friends!`)
-                                                    .then(() => convo.sendTypingIndicator(5000).then(() => connection.estConnection(convo, payload, bot)));
+                                                        ]).then(() => convo.say(`Thanks for creating the username. Your username is ${reqUserName}. Share it among your friends!`)
+                                                            .then(() => convo.sendTypingIndicator(5000).then(() => connection.estConnection(convo, payload, bot))));
+                                                    })
                                             });
                                     });
                             }
