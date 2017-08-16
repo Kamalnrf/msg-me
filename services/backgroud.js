f22d2beaa1c532949fad946a56208ab53fcfcb55
Kamal Mukkamala <Kamalnrf@yahoo.com>
2017-08-17 01:02:45 +0530

    patch

--- services/backgroud.js
+++ services/backgroud.js
@@ -7,7 +7,7 @@
 const msg_me = require('../app/msg-me');
 
 const backGroundService = (bot) => {
-    setInterval(deleteIdleConv, 300000);
+    setInterval(deleteIdleConv, 10000);
 
     console.log("Started all the services.")
 };
@@ -18,11 +18,13 @@ function deleteIdleConv (){
         .then(list => {
             list.split(',')
                 .map(senderID => {
-                    if (senderID !== 'def' || senderID !== 'null')
+                    console.log(`SenderID: ${senderID}`);
                         msg_me.conectedTo(senderID)
                             .then(recieverID => {
+                                console.log(`RecieverID: ${recieverID}`);
                                 msg_me.isIdle(senderID, recieverID)
                                     .then(isIdle => {
+                                        console.log(`Is idle ${isIdle}`);
                                         if (isIdle) {
                                             console.log(`isIdle: ${isIdle}`);
                                             bot.sendTextMessage(senderID, "It seems you are idle for a long time so we are closing the connection");
